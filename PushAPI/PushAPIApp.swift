2723dc0a9034c0c57a852912e4cd741332ff961e
Fong <126961584+dunkeyyfong@users.noreply.github.com>
2023-12-26 21:22:30 +0700

    Initial Commit

---
+++ PushAPI/PushAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushAPIApp.swift
+//  PushAPI
+//
+//  Created by DunkeyyFong on 26/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct PushAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
